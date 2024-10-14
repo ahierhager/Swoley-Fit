d51e22569d0885bee017c42943015ae0886eec66
Anna Hierhager <126461979+ahierhager@users.noreply.github.com>
2024-10-14 12:19:55 +0200

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Swoley-Fit/", // Add this to match your repository name
+  build: {
+    outDir: "build", // This ensures the build folder is named 'build'
+  },
+});
